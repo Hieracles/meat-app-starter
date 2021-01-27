56374b9090d33754fec0c8e20b02079b3c1fd6cf
Hieracles <hieracles@hotmail.com>
2021-01-27 20:04:53 -0300

    Atualizacao

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item/menu-item.model';
+import { CartItem } from './cart-item.model';
+import { ShoppingCartService } from './shopping-cart.services';
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -6,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService : ShoppingCartService) { }
 
   ngOnInit() {
+
+  }
+
+  items() : any[]{
+    return this.shoppingCartService.items;
+  }
+
+  total() : number{
+    return this.shoppingCartService.total();
+  }
+
+  removeItem(item : CartItem){
+    this.shoppingCartService.removeItem(item);
+  } 
+
+  addItem(item : MenuItem){
+    this.shoppingCartService.addItem(item);
+  }  
+
+  clear(){
+    this.shoppingCartService.clear();
   }
 
 }
